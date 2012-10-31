418895e40dfd8461c388d067888e2519b2e88e22
hai.wang <380892880@qq.com>
2012-10-31 20:02:28 +0800

    test gitignore

---
+++ a.c
@@ -0,0 +1,6 @@
+total 4
+drwxr-xr-x    7 aml-wang Administ     4096 Oct 31 19:08 FileInformation
+-rw-r--r--    1 aml-wang Administ        0 Oct 31  2012 a.c
+-rw-r--r--    1 aml-wang Administ       31 Oct 31 19:30 a.o
+-rw-r--r--    1 aml-wang Administ       23 Oct 31 19:08 test
+-rw-r--r--    1 aml-wang Administ      207 Oct 31 19:08 test2
